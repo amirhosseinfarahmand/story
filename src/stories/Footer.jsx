1c97027256cb565d6ebceb9f5d45d84fcbaae5d4
amirhossein <amirnord_ak@yahoo.com>
2022-06-20 15:45:53 +0430

    new

---
+++ src/stories/Footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+  return (
+    <div style={{ display: "flex", width: "100%", backgroundColor: "gray" }}>
+      <ul
+        style={{
+          display: "flex",
+          minWidth: "300px",
+          justifyContent: "space-between",
+        }}
+      >
+        <li>test1</li>
+        <li>test1</li>
+        <li>test1</li>
+      </ul>
+    </div>
+  );
+};
